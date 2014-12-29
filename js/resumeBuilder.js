25542bea559be4e0bef5d51126f360229fc13916
Yegor Kolesnikov <kelegorkel@gmail.com>
2014-12-29 15:38:45 +0200

    incapsulated display funcs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,28 @@ var bio = {
 	},
     "picture": "images\\me.jpg",
     "mes": "God lives!",
-    "skills": ["JS", "HTML", "CSS", "c++", "CUDA"]
+    "skills": ["JS", "HTML", "CSS", "c++", "CUDA"],
+	"display": function () {
+		var fmt_name = HTMLheaderName.replace("%data%", bio.name);
+		var role = HTMLheaderRole.replace("%data%", bio.role);
+		$("#header").append(fmt_name + role);
+
+		var cont_mob = HTMLmobile.replace("%data%", bio.contacts.mobile);
+		var cont_mail = HTMLemail.replace("%data%", bio.contacts.email);
+		var cont_git = HTMLgithub.replace("%data%", bio.contacts.githab);
+		var cont_loc = HTMLlocation.replace("%data%", bio.contacts.location);
+		$("#header").append(cont_mob + cont_mail + cont_git + cont_loc);
+		
+		var pic = HTMLbioPic.replace("%data%", bio.picture);
+		var mes = HTMLWelcomeMsg.replace("%data%", bio.mes);
+		$("#header").append(pic + mes);
+
+		$("#header").append(HTMLskillsStart);
+		for (var i of bio.skills) {
+			var str_skill = HTMLskills.replace("%data%", i);
+			$("#skills").append(str_skill);
+		}
+	}
 };
 
 var education = {
@@ -88,7 +109,19 @@ var work = {
             "dates": "2010-2014",
 			"description": "Modeling, routing and tuning pcbs for radioreceivers. Writing programms for controling, calculating and storring data from receivers using C++, CUDA and OpenGL"
         }
-    ]
+    ],
+	"display": function () {
+		for (var i in work.jobs) {
+			$("#workExperience").append(HTMLworkStart);
+			var empl = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+			var tit = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+			var dates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+			var loc = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+			var desc = HTMLworkDescription.replace("%data%",
+											   work.jobs[i].description);
+			$(".work-entry:last").append(empl+tit+dates+loc+desc);
+		}
+	}
 };
 
 var projects = {
@@ -108,65 +141,26 @@ var projects = {
             "dates": "2012-2014",
             "description": "calculates corfunction for 10000 samples per 1 second"
         }
-    ]
-};
-
-//name and role
-var fmt_name = HTMLheaderName.replace("%data%", bio.name);
-$("#header").append(fmt_name);
-var role = HTMLheaderRole.replace("%data%", bio.role);
-$("#header").append(role);
-
-//contacts
-var cont_mob = HTMLmobile.replace("%data%", bio.contacts.mobile);
-var cont_mail = HTMLemail.replace("%data%", bio.contacts.email);
-var cont_git = HTMLgithub.replace("%data%", bio.contacts.githab);
-var cont_loc = HTMLlocation.replace("%data%", bio.contacts.location);
-$("#header").append(cont_mob);
-$("#header").append(cont_mail);
-$("#header").append(cont_git);
-$("#header").append(cont_loc);
-
-//picture
-var pic = HTMLbioPic.replace("%data%", bio.picture);
-$("#header").append(pic);
-
-//welcome mes
-var mes = HTMLWelcomeMsg.replace("%data%", bio.mes);
-$("#header").append(mes);
-
-//skills
-if (bio.skills !== undefined) {
-	$("#header").append(HTMLskillsStart);
-	var str_skill;
-	for (var i of bio.skills) {
-		str_skill = HTMLskills.replace("%data%", i);
-		$("#skills").append(str_skill);
-	}
-}
-
-//jbos
-function displayWork() {
-	if (work.jobs !== undefined) {
-		var empl;
-		var tit;
-		var dates;
-		var loc;
-		var desc;
-		for (var i in work.jobs) {
-			$("#workExperience").append(HTMLworkStart);
-			empl = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
-			tit = HTMLworkTitle.replace("%data%", work.jobs[i].title);
-			dates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
-			loc = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-			desc = HTMLworkDescription.replace("%data%",
-											   work.jobs[i].description);
-			$(".work-entry:last").append(empl+tit+dates+loc+desc);
+    ],
+	"display": function() {
+		for (var i in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+			var prj_tit = HTMLprojectTitle.replace("%data%",
+												   projects.projects[i].title);
+			var prj_dates = HTMLprojectDates.replace("%data%",
+													 projects.projects[i].dates);
+			var prj_descr =
+				HTMLprojectDescription.replace("%data%",
+											   projects.projects[i].description);
+			$(".project-entry:last").append(prj_tit + prj_dates + prj_descr);
 		}
 	}
-}
+};
 
-displayWork();
+bio.display();
+work.display();
+projects.display();
+education.display();
 
 //click events
 //$(document).click(function(loc) {logClicks(loc.pageX,loc.pageY);});
@@ -181,24 +175,6 @@ function inName(loc_name) {
 		int_name[0].slice(1).toLowerCase();
 	return int_name[0] + " " + int_name[1];
 }
-
-//projects section
-projects.display = function() {
-	for (var i in projects.projects) {
-		$("#projects").append(HTMLprojectStart);
-		var prj_tit = HTMLprojectTitle.replace("%data%",
-											   projects.projects[i].title);
-		var prj_dates = HTMLprojectDates.replace("%data%",
-												 projects.projects[i].dates);
-		var prj_descr =
-			HTMLprojectDescription.replace("%data%",
-										   projects.projects[i].description);
-		$(".project-entry:last").append(prj_tit + prj_dates + prj_descr);
-	}
-}
-projects.display();
-
-education.display();
 
 //map
 $("#mapDiv").append(googleMap);
